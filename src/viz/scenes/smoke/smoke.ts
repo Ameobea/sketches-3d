771b948c4329f68e51dd6a5b50a13b16c58e23d8
Casey Primozic <me@ameo.link>
2023-08-20 15:29:39 -0700

    Checkpoint smoke
     * postprocessing seems to finally be actually working

--- src/viz/scenes/smoke/smoke.ts
+++ src/viz/scenes/smoke/smoke.ts
@@ -29,18 +29,19 @@ export const processLoadedScene = async (viz: VizState, loadedWorld: THREE.Group
   const building = loadedWorld.getObjectByName('Cube') as THREE.Mesh;
   building.material = buildCustomShader(
     {
-      map: buildingTexture,
+      color: new THREE.Color(0x141414),
+      // map: buildingTexture,
       roughness: 0.4,
       metalness: 0.7,
       uvTransform: new THREE.Matrix3().scale(0.82, 0.82),
     },
-    {},
-    { useGeneratedUVs: true, randomizeUVOffset: true, tileBreaking: { type: 'neyret', patchScale: 8 } }
+    {}
+    // { useGeneratedUVs: true, randomizeUVOffset: true, tileBreaking: { type: 'neyret', patchScale: 8 } }
   );
 
   viz.renderer.shadowMap.enabled = true;
   viz.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-  viz.renderer.shadowMap.autoUpdate = true;
+  viz.renderer.shadowMap.needsUpdate = true;
 
   const lightPos = new THREE.Vector3(-32, 27, -32);
 
@@ -48,6 +49,8 @@ export const processLoadedScene = async (viz: VizState, loadedWorld: THREE.Group
   dirLight.target.position.set(22, -2, 10);
   dirLight.castShadow = true;
   dirLight.shadow.bias = 0.005;
+  // dirLight.shadow.blurSamples = 24;
+  // dirLight.shadow.radius = 200;
   dirLight.shadow.mapSize.width = 1024 * 2;
   dirLight.shadow.mapSize.height = 1024 * 2;
   dirLight.shadow.autoUpdate = true;
@@ -66,39 +69,68 @@ export const processLoadedScene = async (viz: VizState, loadedWorld: THREE.Group
 
   viz.scene.fog = new THREE.Fog(LIGHT_COLOR, 0.02, 200);
 
-  // light helper to debug
-  // const dirLightHelper = new THREE.DirectionalLightHelper(dirLight, 5);
-  // viz.scene.add(dirLightHelper);
-
-  // const pointLight = new THREE.PointLight(LIGHT_COLOR, 1, 100);
-  // pointLight.castShadow = true;
-  // // pointLight.shadow.bias = -0.005;
-  // pointLight.shadow.mapSize.width = 1024 * 2;
-  // pointLight.shadow.mapSize.height = 1024 * 2;
-  // pointLight.shadow.autoUpdate = true;
-  // pointLight.shadow.camera.near = 0.1;
-  // pointLight.shadow.camera.far = 200;
-  // pointLight.matrixWorldNeedsUpdate = true;
-  // pointLight.updateMatrixWorld();
-  // pointLight.position.copy(lightPos);
-  // viz.scene.add(pointLight);
-  // const dirLight = pointLight;
-
   // Render the scene once to populate the shadow map
   viz.renderer.render(viz.scene, viz.camera);
 
-  const lightSphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: 0xffff00 })
-  );
-  lightSphere.position.copy(lightPos);
-  lightSphere.castShadow = false;
-  lightSphere.receiveShadow = false;
-  viz.scene.add(lightSphere);
+  // const lightSphere = new THREE.Mesh(
+  //   new THREE.SphereGeometry(0.5, 16, 16),
+  //   new THREE.MeshBasicMaterial({ color: 0xffff00 })
+  // );
+  // lightSphere.position.copy(lightPos);
+  // lightSphere.castShadow = false;
+  // lightSphere.receiveShadow = false;
+  // viz.scene.add(lightSphere);
 
-  const effectComposer = new EffectComposer(viz.renderer);
+  const bgMonoliths = loadedWorld.children.filter(c => c.name.startsWith('bg_monolith')) as THREE.Mesh[];
+  const bgMonolithMaterial = buildCustomShader(
+    { color: new THREE.Color(0xffffff) },
+    {},
+    { enableFog: false }
+  );
+
+  const rectArea = new THREE.RectAreaLight(LIGHT_COLOR, 28.8, 40, 1);
+  rectArea.position.set(-25, 8, 28);
+  rectArea.rotation.set(0, 0, 0);
+  rectArea.lookAt(rectArea.position.x, -rectArea.position.y, rectArea.position.z);
+  viz.scene.add(rectArea);
+
+  const ceilingRailings = loadedWorld.getObjectByName('ceiling_railings') as THREE.Mesh;
+  ceilingRailings.material = buildCustomShader(
+    {
+      color: new THREE.Color(LIGHT_COLOR),
+      metalness: 1,
+    },
+    {},
+    {}
+  );
+
+  configurePostprocessing(viz, dirLight);
+
+  return {
+    spawnLocation: 'spawn',
+    player: {
+      movementAccelPerSecond: { onGround: 9, inAir: 9 },
+      colliderCapsuleSize: { height: 2.2, radius: 0.3 },
+      jumpVelocity: 16,
+    },
+    locations: {
+      spawn: {
+        pos: new THREE.Vector3(0, 0, 0),
+        rot: new THREE.Vector3(-0.01, 1.412, 0),
+      },
+    },
+    // viewMode: {
+    //   type: 'orbit',
+    //   pos: new THREE.Vector3(50, 50, 50),
+    //   target: new THREE.Vector3(0, 0, 0),
+    // },
+    debugPos: true,
+  };
+};
+
+function configurePostprocessing(viz: VizState, dirLight: THREE.DirectionalLight) {
+  const effectComposer = new EffectComposer(viz.renderer, { multisampling: 2 });
   const renderPass = new RenderPass(viz.scene, viz.camera);
-  renderPass.renderToScreen = false;
   effectComposer.addPass(renderPass);
 
   const godraysParams: GodraysPassParams = {
@@ -106,8 +138,8 @@ export const processLoadedScene = async (viz: VizState, loadedWorld: THREE.Group
     edgeRadius: 2,
     edgeStrength: 2,
     distanceAttenuation: 1,
-    density: 1 / 10,
-    maxDensity: 0.88,
+    density: 1 / 22,
+    maxDensity: 1,
     raymarchSteps: 60,
     blur: { kernelSize: KernelSize.LARGE, variance: 0.25 },
   };
@@ -125,7 +157,6 @@ export const processLoadedScene = async (viz: VizState, loadedWorld: THREE.Group
   effectComposer.addPass(n8aoPass);
 
   const godraysEffect = new GodraysPass(dirLight, viz.camera, godraysParams);
-  godraysEffect.renderToScreen = true;
   effectComposer.addPass(godraysEffect);
 
   // const bloomEffect = new BloomEffect({
@@ -137,36 +168,14 @@ export const processLoadedScene = async (viz: VizState, loadedWorld: THREE.Group
   // });
   // const bloomPass = new EffectPass(viz.camera, bloomEffect);
   // bloomPass.dithering = false;
-
   // effectComposer.addPass(bloomPass);
 
   const smaaEffect2 = new SMAAEffect({ preset: SMAAPreset.MEDIUM });
   const smaaPass2 = new EffectPass(viz.camera, smaaEffect2);
-  // smaaPass2.renderToScreen = true;
-  // effectComposer.addPass(smaaPass2);
+  effectComposer.addPass(smaaPass2);
 
   viz.setRenderOverride(timeDiffSeconds => {
     effectComposer.render(timeDiffSeconds);
+    viz.renderer.shadowMap.autoUpdate = false;
   });
-
-  return {
-    spawnLocation: 'spawn',
-    player: {
-      movementAccelPerSecond: { onGround: 9, inAir: 9 },
-      colliderCapsuleSize: { height: 2.2, radius: 0.3 },
-      jumpVelocity: 16,
-    },
-    locations: {
-      spawn: {
-        pos: new THREE.Vector3(0, 0, 0),
-        rot: new THREE.Vector3(),
-      },
-    },
-    // viewMode: {
-    //   type: 'orbit',
-    //   pos: new THREE.Vector3(50, 50, 50),
-    //   target: new THREE.Vector3(0, 0, 0),
-    // },
-    debugPos: true,
-  };
-};
+}
