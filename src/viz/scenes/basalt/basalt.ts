155a84eace188368e90988cc87cd6331ceec1e4c
Casey Primozic <me@ameo.link>
2024-09-09 01:38:05 -0700

    Generate decorative standalone hex pillars in basalt

--- src/viz/scenes/basalt/basalt.ts
+++ src/viz/scenes/basalt/basalt.ts
@@ -90,7 +90,6 @@ const addCrystals = (
 
     const transforms = basaltEngine.basalt_take_crystal_mesh_transforms(ctx, crystalIx);
 
-    // const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
     const instancedMesh = new THREE.InstancedMesh(geometry, mat, transforms.length / 16);
     instancedMesh.castShadow = true;
     instancedMesh.receiveShadow = true;
@@ -119,6 +118,15 @@ export const processLoadedScene = async (
   _loadedWorld: THREE.Group,
   vizConf: VizConfig
 ): Promise<SceneConfig> => {
+  viz.camera.near = 0.07;
+  viz.camera.far = 7000;
+  viz.camera.updateProjectionMatrix();
+
+  const basaltEngineP = import('../../wasmComp/basalt').then(async engine => {
+    await engine.default();
+    return engine;
+  });
+
   const ambientLight = new THREE.AmbientLight(0xffffff, 2.4);
   viz.scene.add(ambientLight);
 
@@ -161,10 +169,7 @@ export const processLoadedScene = async (
   const playCrystalLandSound = () => playCrystalLandSoundInner?.();
 
   // TODO: use web worker
-  const basaltEngine = await import('../../wasmComp/basalt').then(async engine => {
-    await engine.default();
-    return engine;
-  });
+  const basaltEngine = await basaltEngineP;
   const basaltCtx = basaltEngine.basalt_gen();
   const chunkCount = basaltEngine.basalt_get_chunk_count(basaltCtx);
   const geometries = [];
@@ -180,18 +185,6 @@ export const processLoadedScene = async (
     geometry.setIndex(new THREE.BufferAttribute(needsU32Indices ? indices : new Uint16Array(indices), 1));
     geometries.push(geometry);
   }
-  // const displacementNormals = basaltEngine.basalt_take_displacement_normals(basaltCtx);
-
-  // // add arrows to debug displacement normals
-  // for (let i = 0; i < displacementNormals.length; i += 3) {
-  //   const arrow = new THREE.ArrowHelper(
-  //     new THREE.Vector3(displacementNormals[i], displacementNormals[i + 1], displacementNormals[i + 2]),
-  //     new THREE.Vector3(vertices[i], vertices[i + 1], vertices[i + 2]),
-  //     1.5,
-  //     0xff0000
-  //   );
-  //   viz.scene.add(arrow);
-  // }
 
   const collisionIndices = basaltEngine.basalt_take_collision_indices(basaltCtx);
   const collisionVertices = basaltEngine.basalt_take_collision_vertices(basaltCtx);
@@ -208,49 +201,60 @@ export const processLoadedScene = async (
 
   viz.scene.background = bgTexture;
 
-  const mat = buildCustomShader(
+  const pillarParams = {
+    color: 0x372a20,
+    map: glassDiffuseTex,
+    roughnessMap: glassDiffuseTex,
+    roughness: 1,
+    uvTransform: new THREE.Matrix3().scale(0.073, 0.073),
+    normalMap: glassNormalMap,
+    normalScale: 0.85,
+    normalMapType: THREE.TangentSpaceNormalMap,
+    mapDisableDistance: null,
+    metalness: 0.11,
+    iridescence: 0.1,
+  };
+  const pillarShaders = {
+    roughnessShader: `
+    float getCustomRoughness(vec3 pos, vec3 normal, float baseRoughness, float curTimeSeconds, SceneCtx ctx) {
+      return clamp(1. - baseRoughness + 0.242, 0., 1.);
+    }`,
+    iridescenceShader: `
+    float getCustomIridescence(vec3 pos, vec3 normal, float baseIridescence, float curTimeSeconds, SceneCtx ctx) {
+      vec3 noisePos = pos * 0.28 + vec3(curTimeSeconds * 0.004);
+      float noiseVal = pow(fbm(noisePos), 2.);
+      return baseIridescence + smoothstep(0.3, 0.6, noiseVal) * 0.2;
+    }
+    `,
+    colorShader: `
+vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, SceneCtx ctx) {
+float downActivation = 1. - smoothstep(-30., 0., pos.y);
+vec3 lowColor = vec3(0.204,0.153,0.114)*0.03;
+vec3 outColor = mix(baseColor, lowColor, downActivation);
+float noiseVal = clamp(pow(fbm(pos * 0.1) * 2.5 - 1., 1.4), 0., 1.);
+vec3 noiseColor = vec3(0.204,0.153,0.114)*0.03;
+outColor = mix(outColor, noiseColor, noiseVal);
+return vec4(outColor, 1.);
+}
+    `,
+  };
+  const pillarMat = buildCustomShader(pillarParams, pillarShaders, { useTriplanarMapping: true });
+  const distantPillerMat = buildCustomShader(
     {
-      color: 0x372a20,
-      map: glassDiffuseTex,
-      roughnessMap: glassDiffuseTex,
-      roughness: 1,
-      uvTransform: new THREE.Matrix3().scale(0.073, 0.073),
-      normalMap: glassNormalMap,
-      normalScale: 0.85,
-      normalMapType: THREE.TangentSpaceNormalMap,
-      mapDisableDistance: null,
-      metalness: 0.11,
-      iridescence: 0.1,
+      ...pillarParams,
+      color: 0x271a10,
+      uvTransform: new THREE.Matrix3().scale(0.003, 0.003),
+      iridescence: 0,
     },
     {
-      roughnessShader: `
-      float getCustomRoughness(vec3 pos, vec3 normal, float baseRoughness, float curTimeSeconds, SceneCtx ctx) {
-        return clamp(1. - baseRoughness + 0.242, 0., 1.);
-      }`,
-      iridescenceShader: `
-      float getCustomIridescence(vec3 pos, vec3 normal, float baseIridescence, float curTimeSeconds, SceneCtx ctx) {
-        vec3 noisePos = pos * 0.28 + vec3(curTimeSeconds * 0.004);
-        float noiseVal = pow(fbm(noisePos), 2.);
-        return baseIridescence + smoothstep(0.3, 0.6, noiseVal) * 0.2;
-      }
-      `,
-      colorShader: `
-vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, SceneCtx ctx) {
-  float downActivation = 1. - smoothstep(-30., 0., pos.y);
-  vec3 lowColor = vec3(0.204,0.153,0.114)*0.03;
-  vec3 outColor = mix(baseColor, lowColor, downActivation);
-  float noiseVal = clamp(pow(fbm(pos * 0.1) * 2.5 - 1., 1.4), 0., 1.);
-  vec3 noiseColor = vec3(0.204,0.153,0.114)*0.03;
-  outColor = mix(outColor, noiseColor, noiseVal);
-  return vec4(outColor, 1.);
-}
-      `,
+      ...pillarShaders,
+      iridescenceShader: undefined,
     },
     { useTriplanarMapping: true }
   );
-  viz.registerBeforeRenderCb(curTimeSeconds => mat.setCurTimeSeconds(curTimeSeconds));
+  viz.registerBeforeRenderCb(curTimeSeconds => pillarMat.setCurTimeSeconds(curTimeSeconds));
   for (const geometry of geometries) {
-    const mesh = new THREE.Mesh(geometry, mat);
+    const mesh = new THREE.Mesh(geometry, pillarMat);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     mesh.position.set(0, 0, 0);
@@ -260,7 +264,6 @@ vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, S
   const crystalMat = buildCustomShader(
     {
       color: new THREE.Color(0x24170d),
-      // color: 0x3d0404,
       metalness: 0.89,
       roughness: 0.312,
       ambientLightScale: 2.4,
@@ -279,7 +282,28 @@ vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, S
   viz.registerBeforeRenderCb(curTimeSeconds => crystalMat.setCurTimeSeconds(curTimeSeconds));
 
   addCrystals(viz, basaltEngine, basaltCtx, crystalMat);
+
+  const standalonePillarIndices = basaltEngine.basalt_take_standalone_pillar_mesh_indices(basaltCtx);
+  const standalonePillarVertices = basaltEngine.basalt_take_standalone_pillar_mesh_vertices(basaltCtx);
+  const standalonePillarNormals = basaltEngine.basalt_take_standalone_pillar_mesh_normals(basaltCtx);
+  const standalonePillarTransforms =
+    basaltEngine.basalt_take_standalone_pillar_instance_transforms(basaltCtx);
   basaltEngine.basalt_free(basaltCtx);
+
+  const standalonePillarGeometry = new THREE.BufferGeometry();
+  standalonePillarGeometry.setAttribute('position', new THREE.BufferAttribute(standalonePillarVertices, 3));
+  standalonePillarGeometry.setAttribute('normal', new THREE.BufferAttribute(standalonePillarNormals, 3));
+  standalonePillarGeometry.setIndex(new THREE.BufferAttribute(standalonePillarIndices, 1));
+
+  const standalonePillars = new THREE.InstancedMesh(
+    standalonePillarGeometry,
+    distantPillerMat,
+    standalonePillarTransforms.length / 16
+  );
+  standalonePillars.instanceMatrix.set(standalonePillarTransforms);
+  standalonePillars.castShadow = false;
+  standalonePillars.receiveShadow = false;
+  viz.scene.add(standalonePillars);
 
   const dirLight = new THREE.DirectionalLight(0xffe6e1, 3.6);
   dirLight.position.set(142 * 1.1, 65 * 1.1, 380 * 1.1);
@@ -313,6 +337,25 @@ vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, S
     viz,
     vizConf.graphics.quality,
     (composer, viz, quality) => {
+      const n8aoPass = new N8AOPostPass(
+        viz.scene,
+        viz.camera,
+        viz.renderer.domElement.width,
+        viz.renderer.domElement.height
+      );
+      composer.addPass(n8aoPass);
+      n8aoPass.gammaCorrection = false;
+      n8aoPass.configuration.intensity = 2;
+      n8aoPass.configuration.aoRadius = 5;
+      // n8aoPass.configuration.halfRes = vizConf.graphics.quality <= GraphicsQuality.Low;
+      n8aoPass.setQualityMode(
+        {
+          [GraphicsQuality.Low]: 'Low',
+          [GraphicsQuality.Medium]: 'Low',
+          [GraphicsQuality.High]: 'High',
+        }[vizConf.graphics.quality]
+      );
+
       const volumetricPass = new VolumetricPass(viz.scene, viz.camera, {
         fogMinY: -60,
         fogMaxY: -40,
@@ -343,25 +386,6 @@ vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, S
       });
       composer.addPass(volumetricPass);
       viz.registerBeforeRenderCb(curTimeSeconds => volumetricPass.setCurTimeSeconds(curTimeSeconds));
-
-      const n8aoPass = new N8AOPostPass(
-        viz.scene,
-        viz.camera,
-        viz.renderer.domElement.width,
-        viz.renderer.domElement.height
-      );
-      composer.addPass(n8aoPass);
-      n8aoPass.gammaCorrection = false;
-      n8aoPass.configuration.intensity = 2;
-      n8aoPass.configuration.aoRadius = 5;
-      // n8aoPass.configuration.halfRes = vizConf.graphics.quality <= GraphicsQuality.Low;
-      n8aoPass.setQualityMode(
-        {
-          [GraphicsQuality.Low]: 'Low',
-          [GraphicsQuality.Medium]: 'Low',
-          [GraphicsQuality.High]: 'High',
-        }[vizConf.graphics.quality]
-      );
     },
     undefined,
     { toneMappingExposure: 1.48 },
@@ -371,7 +395,6 @@ vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, S
       });
 
       return [toneMappingEffect];
-      return [];
     })()
   );
 
@@ -382,7 +405,7 @@ vec4 getFragColor(vec3 baseColor, vec3 pos, vec3 normal, float curTimeSeconds, S
       moveSpeed: { onGround: 10, inAir: 13 },
       colliderCapsuleSize: { height: 2.2, radius: 0.8 },
       jumpVelocity: 12,
-      oobYThreshold: -10,
+      oobYThreshold: -20,
       dashConfig: {
         enable: true,
       },
--- src/viz/wasm/basalt/src/lib.rs
+++ src/viz/wasm/basalt/src/lib.rs
@@ -1,15 +1,17 @@
 #![feature(array_chunks, iter_array_chunks, array_windows)]
 
 use bitvec::bitarr;
-use common::{random, smoothstep, uninit};
+use common::{random, rng, smoothstep, uninit};
 use crystals::{generate_crystals, BatchMesh};
+use fxhash::FxHashSet;
 use log::info;
 use mesh::{
-  linked_mesh::{set_debug_print, DisplacementNormalMethod, Edge, Face},
+  linked_mesh::{set_debug_print, DisplacementNormalMethod, Edge, Face, Mat4, Vec3},
   LinkedMesh, OwnedIndexedMesh, Triangle,
 };
 use nalgebra::Vector3;
 use noise::{self, Fbm, MultiFractal, NoiseModule, Seedable};
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 
 mod crystals;
@@ -88,7 +90,6 @@ fn connect_hexes(
       Triangle::new(get_vtx(RIGHT, 2), get_vtx(RIGHT, 1), get_vtx(LEFT, 1)),
       Triangle::new(get_vtx(RIGHT, 2), get_vtx(LEFT, 1), get_vtx(LEFT, 2)),
     ],
-    // _ => unreachable!(),
     _ => return,
   };
 
@@ -139,12 +140,23 @@ pub(crate) fn gen_hex_triangles(
   })
 }
 
-/// Generates a tessellated hex grid centered at the origin.  Each hex is
-/// extruded dynamically based on the height returned by `get_hex_height` at its
-/// center.
+fn get_hex_center_coords(
+  y_ix: isize,
+  x_ix: isize,
+  hex_width: f32,
+  hex_height_z: f32,
+) -> (f32, f32) {
+  let hex_center_x = x_ix as f32 * 1.5 * hex_width;
+  let hex_center_z =
+    y_ix as f32 * 2.0 * hex_height_z + if x_ix % 2 == 0 { 0. } else { hex_height_z };
+  (hex_center_x, hex_center_z)
+}
+
+/// Generates a hex grid centered at the origin.  Each hex is extruded dynamically based on the
+/// height returned by `get_hex_height` at its center.
 ///
 /// `hex_width` corresponds to the distance between the two parallel flat sides.
-fn gen_tessellated_hex_grid(
+fn gen_hex_grid(
   x_count: usize,
   y_count: usize,
   hex_width: f32,
@@ -156,16 +168,10 @@ fn gen_tessellated_hex_grid(
   let mut hex_heights = Vec::with_capacity(x_count * y_count);
   let mut void_flags = vec![bitarr![0; 1024]; y_count];
 
-  let get_hex_center_coords = |y_ix: usize, x_ix: usize| -> (f32, f32) {
-    let hex_center_x = x_ix as f32 * 1.5 * hex_width;
-    let hex_center_z =
-      y_ix as f32 * 2.0 * hex_height_z + if x_ix % 2 == 0 { 0. } else { hex_height_z };
-    (hex_center_x, hex_center_z)
-  };
-
   for y in 0..y_count {
     for x in 0..x_count {
-      let (hex_center_x, hex_center_z) = get_hex_center_coords(y, x);
+      let (hex_center_x, hex_center_z) =
+        get_hex_center_coords(y as isize, x as isize, hex_width, hex_height_z);
       // keep the outer ring clear to ensure that no hexes have missing sides
       let hex_height = if x == 0 || y == 0 || x == x_count - 1 || y == y_count - 1 {
         -1000.
@@ -338,7 +344,12 @@ fn gen_tessellated_hex_grid(
 
   let triangles_per_hex = 6;
   let mut tri_ix = 0;
-  triangles.retain(|_| {
+  triangles.retain(|tri| {
+    if tri.a.y < -10. && tri.b.y < -10. && tri.c.y < -10. {
+      tri_ix += 1;
+      return false;
+    }
+
     let hex_ix = tri_ix / triangles_per_hex;
     if hex_ix >= x_count * y_count {
       return true;
@@ -356,10 +367,18 @@ fn gen_tessellated_hex_grid(
   triangles
 }
 
+/// Generates a single hex pillar including the top and all side faces
+fn gen_disconnected_hex_pillar(height: f32, hex_width: f32) -> Vec<Triangle> {
+  gen_hex_grid(3, 3, hex_width, |_, _| height)
+}
+
 pub struct GenBasaltCtx {
   pub collission_mesh: OwnedIndexedMesh,
-  pub terrrain_mesh_chunks: Vec<OwnedIndexedMesh>,
+  pub terrain_mesh_chunks: Vec<OwnedIndexedMesh>,
   pub(crate) crystals: Vec<BatchMesh>,
+  pub standalone_pillar_mesh: OwnedIndexedMesh,
+  /// packed 4x4 matrices
+  pub standalone_pillar_instance_transforms: Vec<f32>,
 }
 
 static mut DID_INIT: bool = false;
@@ -406,6 +425,74 @@ fn displace_mesh(noise: &Fbm<f32>, mesh: &mut LinkedMesh) {
   }
 }
 
+fn gen_standalone_pillar_mesh(hex_width: f32, hex_height: f32) -> OwnedIndexedMesh {
+  let mut mesh: LinkedMesh<()> =
+    LinkedMesh::from_triangles(gen_disconnected_hex_pillar(hex_height, hex_width));
+  mesh.merge_vertices_by_distance(0.0001);
+  mesh.compute_vertex_displacement_normals();
+  mesh.mark_edge_sharpness(0.8);
+  mesh.separate_vertices_and_compute_normals();
+  mesh.to_raw_indexed(true, false)
+}
+
+fn gen_stand_alone_pillar_instance_transforms(instance_count: usize, hex_width: f32) -> Vec<f32> {
+  // avoid placing pillars into the same or overlapping spots
+  let mut out_transforms = Vec::with_capacity(instance_count * 16);
+  let mut placed_coords: FxHashSet<(isize, isize)> = FxHashSet::default();
+
+  let hex_height_z = (3.0_f32).sqrt() * hex_width / 2.;
+  let coords_are_valid = |x: isize, y: isize| -> bool {
+    let hex_center = get_hex_center_coords(y, x, hex_width, hex_height_z);
+    if (hex_center.0 > -40. && hex_center.0 < 450.) && (hex_center.1 > -120. && hex_center.1 < 450.)
+    {
+      return false;
+    }
+
+    if (x.abs() > 4 || y.abs() > 4) && rng().gen_range(0.0..1.) < 0.8 {
+      return false;
+    }
+
+    return true;
+  };
+
+  let (offset_x, offset_z) = get_hex_center_coords(-1, -1, hex_width, hex_height_z);
+  for _ in 0..instance_count {
+    let (x, y) = loop {
+      let mut y = rng().gen_range(-19..19);
+      let mut x = rng().gen_range(-19..19);
+      while !coords_are_valid(x, y) {
+        y = rng().gen_range(-15..15);
+        x = rng().gen_range(-15..15);
+      }
+
+      let is_new = placed_coords.insert((x, y));
+      if is_new {
+        break (x, y);
+      }
+    };
+
+    let hex_height_z = (3.0_f32).sqrt() * hex_width / 2.;
+    let hex_center = get_hex_center_coords(y, x, hex_width, hex_height_z);
+
+    let mut transform = Mat4::identity();
+    let hex_distance = (x.abs() + y.abs()) as f32;
+    let mut y = -450. + (hex_distance * 30.).max(100.);
+    y *= rng().gen_range(0.2..0.9);
+
+    transform.append_translation_mut(&Vec3::new(
+      hex_center.0 + offset_x,
+      y,
+      hex_center.1 + offset_z,
+    ));
+    let scale = 1. + hex_distance * 0.1;
+    transform.append_scaling_mut(scale);
+
+    out_transforms.extend(transform.as_slice());
+  }
+
+  out_transforms
+}
+
 #[wasm_bindgen]
 pub fn basalt_gen() -> *mut GenBasaltCtx {
   maybe_init();
@@ -425,7 +512,7 @@ pub fn basalt_gen() -> *mut GenBasaltCtx {
     }
     height
   };
-  let terrain_triangles = gen_tessellated_hex_grid(25, 25, 11., get_height);
+  let terrain_triangles = gen_hex_grid(25, 25, 11.5, get_height);
 
   let mut mesh = LinkedMesh::from_triangles(terrain_triangles);
   let merged_count = mesh.merge_vertices_by_distance(0.0001);
@@ -443,7 +530,7 @@ pub fn basalt_gen() -> *mut GenBasaltCtx {
   displace_mesh(&displ_noise, &mut collission_mesh);
   let collission_mesh = mesh.to_raw_indexed(false, false);
 
-  let target_edge_length = 3.16;
+  let target_edge_length = 4.26;
   let should_split_edge = |mesh: &LinkedMesh, edge: &Edge| -> bool {
     // avoid splitting edges that are low down to save resources since they will be
     // far from the player and not easily visible
@@ -481,65 +568,62 @@ pub fn basalt_gen() -> *mut GenBasaltCtx {
   );
 
   let crystals = generate_crystals(&mesh);
+  let terrain_mesh_chunks =
+    mesh.to_raw_indexed_multi(true, false, |face: &Face<()>| -> (isize, isize) {
+      let mins: (f32, f32) = face
+        .iter_vtx_positions(&mesh.vertices)
+        .fold((f32::MAX, f32::MAX), |(min_x, min_z), pos| {
+          (min_x.min(pos.x), min_z.min(pos.z))
+        });
+
+      let chunk_size = 50.;
+      let chunk_x = (mins.0 / chunk_size).floor() as isize;
+      let chunk_z = (mins.1 / chunk_size).floor() as isize;
+      (chunk_x, chunk_z)
+    });
+
+  let standalone_hex_width = 38.5;
+  let standalone_hex_height = 240.;
+  let standalone_pillar_mesh =
+    gen_standalone_pillar_mesh(standalone_hex_width, standalone_hex_height);
+  let standalone_pillar_instance_transforms =
+    gen_stand_alone_pillar_instance_transforms(80, standalone_hex_width);
+
   Box::into_raw(Box::new(GenBasaltCtx {
     collission_mesh,
-    terrrain_mesh_chunks: mesh.to_raw_indexed_multi(
-      true,
-      false,
-      |face: &Face<()>| -> (isize, isize) {
-        let mins: (f32, f32) = face
-          .iter_vtx_positions(&mesh.vertices)
-          .fold((f32::MAX, f32::MAX), |(min_x, min_z), pos| {
-            (min_x.min(pos.x), min_z.min(pos.z))
-          });
-
-        let chunk_size = 50.;
-        let chunk_x = (mins.0 / chunk_size).floor() as isize;
-        let chunk_z = (mins.1 / chunk_size).floor() as isize;
-        (chunk_x, chunk_z)
-      },
-    ),
+    terrain_mesh_chunks,
     crystals,
+    standalone_pillar_mesh,
+    standalone_pillar_instance_transforms,
   }))
 }
 
 #[wasm_bindgen]
 pub fn basalt_get_chunk_count(ctx: *mut GenBasaltCtx) -> usize {
   let ctx = unsafe { &mut (*ctx) };
-  ctx.terrrain_mesh_chunks.len()
+  ctx.terrain_mesh_chunks.len()
 }
 
 #[wasm_bindgen]
 pub fn basalt_take_vertices(ctx: *mut GenBasaltCtx, chunk_ix: usize) -> Vec<f32> {
   let ctx = unsafe { &mut (*ctx) };
-  std::mem::take(&mut ctx.terrrain_mesh_chunks[chunk_ix].vertices)
+  std::mem::take(&mut ctx.terrain_mesh_chunks[chunk_ix].vertices)
 }
 
 #[wasm_bindgen]
 pub fn basalt_take_indices(ctx: *mut GenBasaltCtx, chunk_ix: usize) -> Vec<usize> {
   let ctx = unsafe { &mut (*ctx) };
-  std::mem::take(&mut ctx.terrrain_mesh_chunks[chunk_ix].indices)
+  std::mem::take(&mut ctx.terrain_mesh_chunks[chunk_ix].indices)
 }
 
 #[wasm_bindgen]
 pub fn basalt_take_normals(ctx: *mut GenBasaltCtx, chunk_ix: usize) -> Vec<f32> {
   let ctx = unsafe { &mut (*ctx) };
   std::mem::take(
-    &mut ctx.terrrain_mesh_chunks[chunk_ix]
+    &mut ctx.terrain_mesh_chunks[chunk_ix]
       .shading_normals
       .as_mut()
       .expect("Shading normals not found"),
-  )
-}
-
-#[wasm_bindgen]
-pub fn basalt_take_displacement_normals(ctx: *mut GenBasaltCtx, chunk_ix: usize) -> Vec<f32> {
-  let ctx = unsafe { &mut (*ctx) };
-  std::mem::take(
-    &mut ctx.terrrain_mesh_chunks[chunk_ix]
-      .displacement_normals
-      .as_mut()
-      .expect("Displacement normals not found"),
   )
 }
 
@@ -597,4 +681,28 @@ pub fn basalt_take_crystal_mesh_transforms(ctx: *mut GenBasaltCtx, crystal_ix: u
   };
   std::mem::forget(transforms);
   raw_transforms
+}
+
+#[wasm_bindgen]
+pub fn basalt_take_standalone_pillar_mesh_indices(ctx: *mut GenBasaltCtx) -> Vec<usize> {
+  let ctx = unsafe { &mut (*ctx) };
+  std::mem::take(&mut ctx.standalone_pillar_mesh.indices)
+}
+
+#[wasm_bindgen]
+pub fn basalt_take_standalone_pillar_mesh_vertices(ctx: *mut GenBasaltCtx) -> Vec<f32> {
+  let ctx = unsafe { &mut (*ctx) };
+  std::mem::take(&mut ctx.standalone_pillar_mesh.vertices)
+}
+
+#[wasm_bindgen]
+pub fn basalt_take_standalone_pillar_mesh_normals(ctx: *mut GenBasaltCtx) -> Vec<f32> {
+  let ctx = unsafe { &mut (*ctx) };
+  std::mem::take(&mut ctx.standalone_pillar_mesh.shading_normals).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn basalt_take_standalone_pillar_instance_transforms(ctx: *mut GenBasaltCtx) -> Vec<f32> {
+  let ctx = unsafe { &mut (*ctx) };
+  std::mem::take(&mut ctx.standalone_pillar_instance_transforms)
 }
